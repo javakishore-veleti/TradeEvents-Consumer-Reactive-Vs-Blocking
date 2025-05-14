2cd59ea92f391711b50d67a070c26b31176f8461
Kishore <>
2025-05-14 08:13:49 -0400

    Initial Commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# TradeEvents-Consumer-Reactive-Vs-Blocking
-A comparative Spring Boot project demonstrating trade event consumption from Kafka using both blocking (Spring MVC + JPA) and non-blocking (Spring WebFlux + R2DBC) approaches, with shared domain and service logic
---
+++ src/main/java/com/jk/apps/trades/reactive_non_reactive/AppMain.java
@@ -0,0 +1,13 @@
+package com.jk.apps.trades.reactive_non_reactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMain.class, args);
+	}
+
+}
